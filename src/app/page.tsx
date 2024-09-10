7536887d6eecd76f79b3d5572cbeb8293b2ccdab
AkshatBanzal <akshatmbanzal@gmail.com>
2024-09-10 14:24:17 +0000

    commit4

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+    return (
+        <main style={{ backgroundColor: '#d6dcdc', display: 'flex', justifyContent: 'center', alignItems: 'center', height: '90vh' }}>
+            <i>Page content</i>
+        </main>
+    );
+}
